19ef4c90c47e12b7ebb7169b49e92c7890e8c5f4
Patrick Wallace <pwa@osx-pwa-n-us.local>
2020-12-23 12:43:51 -0700

    New

--- pages/products/skis/[slug].js
+++ pages/products/skis/[slug].js
@@ -64,7 +64,8 @@ export default function Post({ ski, moreSkis, preview }) {
         )}
       </Container>
       <div>
-        {/* <SupportingImages images={ski.supportingImages}></SupportingImages> */}
+        
+        {/*    <SupportingImages images={ski.supportingImages}></SupportingImages> */}
       </div>
       <SectionSeparatorNoLine></SectionSeparatorNoLine>
       <Container>
