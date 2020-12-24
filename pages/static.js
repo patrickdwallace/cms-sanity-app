3bc0363493a5f306644c9e3804a307767f97853e
Patrick Wallace <pwa@osx-pwa-n-us.local>
2020-12-23 19:24:13 -0700

    12232020-724

---
+++ pages/static.js
@@ -0,0 +1,31 @@
+import { useRouter } from 'next/router'
+import { getAllSkisBySlug, getSkiAndMoreSkis } from 'lib/api'
+
+
+export default function Static({ ski, moreSkis, preview }) {
+  
+  const router = useRouter()
+  
+  if (!router.isFallback && !ski?.slug) {
+    return <ErrorPage statusCode={404} />
+  }
+  return (
+      <div>
+          {ski.title}
+
+      </div>
+
+  )
+}
+
+export async function getStaticProps({ params, preview = false }) {
+  const data = await getSkiAndMoreSkis('blaze-94', preview)
+  return {
+    props: {
+      preview,
+      ski: data?.ski || null,
+      moreSkis: data?.moreSkis || null,
+    },
+  }
+}
+
